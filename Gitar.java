e81081f91202d3d1803b90329e90af55af96d54f
elvirashd <ebulcantik@gmail.com>
2024-09-03 15:19:42 +0700

    Add files via upload

---
+++ Gitar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainmusik;
+
+/**
+ *
+ * @author Nama: : elvira solikha H.I(09040623057)
+ */
+class Gitar implements MainMusik {
+
+    public void main() {
+        System.out.println("Memainkan gitar dengan memetik senarnya");
+    }
+}
---
+++ MainMusik.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mainmusik;
+
+/**
+ *
+ * @author Nama: : elvira solikha H.I(09040623057)
+ */
+interface MainMusik {
+
+void main();  
+
+}
